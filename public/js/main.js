b7a0e6984f035be8462422d4186da79b97a3491b
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-06-05 10:53:53 -0500

    [add] icomoons, images and finish smartphone mode

---
+++ public/js/main.js
@@ -0,0 +1,65 @@
+$(document).ready(function () {
+	// Code for text-decoration out in tags "a"
+	$('a').click(function () {
+		$(this).css('text-decoration', 'none')
+	})
+
+	/*Function show Section Contact*/
+	$('.Button-contact').on('click', function (e) {
+		e.preventDefault();
+		$('.Contact').css('display', 'block');
+		setTimeout(function () {
+			$('.Contact-content').css('display', 'block');
+		}, 400)
+		$('.blanketBody').css('display', 'block');
+	})
+
+	/*Function hide Section Contact*/
+	$('.Contact-buttonCancel').on('click', function (e) {
+		e.preventDefault();
+		$('.Contact').fadeOut('slow');
+		$('.Contact-content').fadeOut('slow');
+		$('.blanketBody').fadeOut('fast');
+	})
+
+	/*Function show Section Menu*/
+	$('.Header-menuButton').on('click', function (e) {
+		e.preventDefault();
+		$('.contentMenu').css('animation-name','showMenu');
+		$('.blanketBody').fadeIn('fast');
+		$('.contentMenu').css({
+			'display' : 'block'
+		});
+	})
+
+	/*Function hide Section Menu*/
+	$('.blanketBody').on('click', function () {
+		$('.contentMenu').css('animation-name','hideMenu');
+		setTimeout(function () {
+			$('.contentMenu').css('display', 'none');
+		}, 600)
+		$('.Contact').fadeOut('slow');
+		$('.Contact-content').fadeOut('slow');
+		$(this).fadeOut('slow');
+	})
+
+	/*Function for show Sections according listItems clicked*/
+	$('.listMenu li a').on('click', function (e) {
+		e.preventDefault();
+		$('section').fadeOut('fast');
+		$(this.hash).fadeIn('fast');
+		$('.listMenu li a').each(function (i) {
+			this.style.color = "#333"
+			this.style.marginLeft = "0"
+		})
+		$(this).css({
+			'color' : 'Teal',
+			'margin-left' : '10px'
+		})
+		$('.contentMenu').css('animation-name','hideMenu');
+		setTimeout(function () {
+			$('.contentMenu').css('display', 'none');
+		}, 600)
+		$('.blanketBody').fadeOut('fast');
+	})
+})
