902427e26f8e4fe57694a8c4e97a3b6be6503d1c
John Baquero Celis <jfbaquerocelis@gmail.com>
2015-06-11 15:18:34 -0500

    [update] desktop mode and resolved bugs

--- public/js/main.js
+++ public/js/main.js
@@ -2,6 +2,28 @@ $(document).ready(function () {
 	if ($(window).height() < 400){
 		alert('Recomendación: Rota tu Smartphone para mejor experiencia')
 	}
+	// Code for text-decoration out in tags "a"
+	$('a').click(function () {
+		$(this).css('text-decoration', 'none')
+	})
+
+	/*Function show Section Contact*/
+	$('.Button-contact').on('click', function (e) {
+		e.preventDefault();
+		setTimeout(function () {
+			$('.Contact-content').fadeIn('fast');
+		}, 400)
+		$('.blanketBody').fadeIn('fast');
+		$('.Contact').fadeIn('fast');
+	})
+
+	/*Function hide Section Contact*/
+	$('.Contact-buttonCancel').on('click', function (e) {
+		e.preventDefault();
+		$('.Contact').fadeOut('slow');
+		$('.Contact-content').fadeOut('slow');
+		$('.blanketBody').fadeOut('fast');
+	})
 
 	/*Code Javascript for Smartphones*/
 	if (window.matchMedia( "(max-width: 699px)" ).matches){
@@ -9,42 +31,17 @@ $(document).ready(function () {
 		$(window).scroll(function () {
 			if ($(window).scrollTop() > 15){
 				$('.Header-contentButton').css({
-						'position' : 'fixed',
-						'width' : '100%',
-						'top' : '0',
-						'z-index' : '1'
+					'position' : 'fixed',
+					'width' : '100%',
+					'top' : '0',
+					'z-index' : '1'
 				})
 			}else{
 				$('.Header-contentButton').css({
 					'position' : 'initial',
-					'display' : 'block',
+					'display' : 'block'
 				})
 			}
-		})
-
-		// Code for text-decoration out in tags "a"
-		$('a').click(function () {
-			$(this).css('text-decoration', 'none')
-		})
-
-		/*Function show Section Contact*/
-		$('.Button-contact').on('click', function (e) {
-			setTimeout(function () {
-				$('.Contact-content').fadeIn('fast');
-			}, 400)
-			$('.blanketBody').fadeIn('fast');
-			$('.Contact').fadeIn('fast');
-			$('body').animate({
-				scrollTop : 0
-			}, 400)
-		})
-
-		/*Function hide Section Contact*/
-		$('.Contact-buttonCancel').on('click', function (e) {
-			e.preventDefault();
-			$('.Contact').fadeOut('slow');
-			$('.Contact-content').fadeOut('slow');
-			$('.blanketBody').fadeOut('fast');
 		})
 
 		/*Function show Section Menu*/
@@ -90,7 +87,7 @@ $(document).ready(function () {
 		})
 	}
 	/*Code Javascript for Tablets*/
-	if (window.matchMedia( "(min-width: 700px)" ).matches){
+	if (window.matchMedia( "(min-width: 700px)" ).matches && window.matchMedia( "(max-width: 999px)" ).matches){
 		// Fixed contentMenu
 		$(window).scroll(function () {
 			if ($(window).scrollTop() > 15){
@@ -106,29 +103,11 @@ $(document).ready(function () {
 			}
 		})
 
-		// Code for text-decoration out in tags "a"
-		$('a').click(function () {
-			$(this).css('text-decoration', 'none')
-		})
-
-		/*Function show Section Contact*/
-		$('.Button-contact').on('click', function (e) {
-			setTimeout(function () {
-				$('.Contact-content').fadeIn('fast');
-			}, 400)
-			$('.blanketBody').fadeIn('fast');
-			$('.Contact').fadeIn('fast');
-			$('body').animate({
-				scrollTop : 0
-			}, 400)
-		})
-
 		/*Function hide Section Contact*/
-		$('.Contact-buttonCancel').on('click', function (e) {
-			e.preventDefault();
+		$('.blanketBody').on('click', function () {
 			$('.Contact').fadeOut('slow');
 			$('.Contact-content').fadeOut('slow');
-			$('.blanketBody').fadeOut('fast');
+			$(this).fadeOut('slow');
 		})
 
 		/*Function for show Sections according listItems clicked*/
@@ -145,4 +124,46 @@ $(document).ready(function () {
 		})
 	}
 
+	/*Code Javascript for Desktop*/
+	if (window.matchMedia( "(min-width: 1000px)" ).matches){
+		// Fixed contentMenu
+		$(window).scroll(function () {
+			if ($(window).scrollTop() > 15){
+				$('.contentMenu').css({
+					'position' : 'fixed',
+					'height' : '56px'
+				})
+			}else{
+				$('.contentMenu').css({
+					'position' : 'initial',
+					'display' : 'block',
+				})
+			}
+		})
+
+		/*Function hide Section Contact*/
+		$('.blanketBody').on('click', function () {
+			$('.Contact').fadeOut('slow');
+			$('.Contact-content').fadeOut('slow');
+			$(this).fadeOut('slow');
+		})
+
+		/*Function for show Sections according listItems clicked*/
+		$('.listMenu li a').on('click', function (e) {
+			e.preventDefault()
+			$('body, html').animate({
+				scrollTop : $(this.hash).offset().top - 50
+			}, 1000)
+			$('.listMenu li a').each(function (i) {
+				this.style.color = "white"
+			})
+			$(this).css({
+				'color' : '#00E676',
+			})
+		})
+	}
+	/*
+		Fataría agregar trabajos al portafolio y arreglar detallitos como el background de header
+		y la foto mía. entre otras cosas que nos vallamos dando cuenta
+	*/
 })
